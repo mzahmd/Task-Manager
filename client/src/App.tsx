fc1b943ebe44e24c2ef97bbe624bee29cbe9964f
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-25 20:29:20 +0200

    installed tailwind and add Task Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import Tasks from "./components/Tasks"
+
 function App() {
-
   return (
-    <>
-
-    </>
+    <div className="text-center">
+      <h1 className="text-xl font-bold">Todo - List</h1>
+      <Tasks />
+    </div>
   )
 }
 
---
+++ client/src/components/Tasks.tsx
@@ -0,0 +1,8 @@
+export default function Tasks() {
+  return (
+    <div className="mt-5">
+      <input type="text" className="shadow border border-slate-300 p-1" defaultValue=""/>
+      <button className="font-bold bg-emerald-500 hover:bg-emerald-700 rounded text-slate-200 shadow ms-2 py-1 px-2" type="button">ADD</button>
+    </div>
+  )
+}

01bd5400f88f1e7609dff89109309dbbdee687d8
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-25 21:24:39 +0200

    add margin top

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import Tasks from "./components/Tasks"
 
 function App() {
   return (
-    <div className="text-center">
+    <div className="text-center mt-5">
       <h1 className="text-xl font-bold">Todo - List</h1>
       <Tasks />
     </div>

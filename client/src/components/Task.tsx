0943676b2c238d82d08ccaf48de246ab2bec3826
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-26 08:35:56 +0200

    add icons and styled the task card

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,13 +1,15 @@
+import { FaPencilAlt, FaTrash  } from "react-icons/fa";
+
 interface Props {
   task: { name: string }
 }
 
 export default function Task({ task }: Props) {
   return (
-    <div className="flex justify-center flex-row my-2">
-      <span>{task.name}</span>
-      <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 py-1 px-1" type="button">Edit</button>
-      <button className="font-bold bg-red-500 hover:bg-red-700 rounded text-slate-200 shadow ms-2 py-1 px-1" type="button">Delete</button>
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mx-auto my-2">
+      <input type="text" className="p-1" defaultValue={task.name} readOnly/>
+      <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button"><FaPencilAlt /></button>
+      <button className="font-bold bg-red-500 hover:bg-red-700 rounded text-slate-200 shadow ms-2 p-2" type="button"><FaTrash /></button>
     </div>
   )
 }

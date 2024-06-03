6597fd51448d103240f13ae34e0495fb950b00c2
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-06-03 17:59:01 +0200

    editing

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -1,4 +1,5 @@
-import { FaPencilAlt, FaTrash } from "react-icons/fa";
+import { useState } from "react";
+import { FaPencilAlt, FaRegSave, FaTrash } from "react-icons/fa";
 import { useMutation } from "urql";
 import { DeleteTaskMutation } from "../lib/queries";
 
@@ -7,21 +8,28 @@ interface Props {
 }
 
 export default function TaskCard({ task }: Props) {
-  const [_, deleteTask] = useMutation(DeleteTaskMutation)
+  const [_, deleteTask] = useMutation(DeleteTaskMutation);
+  const [isEditing, setIsEditing] = useState(false);
 
-  function handleEdit() {
-
+  function handleEdit(isEdit: boolean) {
+    setIsEditing(isEdit);
   }
 
   async function handleDelete(id: string) {
-    await deleteTask({ id })
+    await deleteTask({ id });
   }
 
   return (
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mx-auto my-2">
-      <input type="text" className="p-1" defaultValue={task.name} readOnly />
-      <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEdit()}><FaPencilAlt /></button>
-      <button className="font-bold bg-red-500 hover:bg-red-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleDelete(task.id)}><FaTrash /></button>
+      <input type="text" className="p-1" defaultValue={task.name} readOnly={!isEditing} />
+      {isEditing ?
+        <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEdit(false)}><FaRegSave /></button>
+        :
+        <>
+          <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEdit(true)}><FaPencilAlt /></button>
+          <button className="font-bold bg-red-500 hover:bg-red-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleDelete(task.id)}><FaTrash /></button>
+        </>
+      }
     </div>
   )
 }

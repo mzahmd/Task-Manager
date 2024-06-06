743c38d644982eb9c55f084fda4a2a60fdc96aeb
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-06-06 19:46:43 +0200

    types

--- client/src/graphql-env.d.ts
+++ client/src/graphql-env.d.ts
@@ -1,18 +1,33 @@
+/* eslint-disable */
+/* prettier-ignore */
+
+/** An IntrospectionQuery representation of your schema.
+ *
+ * @remarks
+ * This is an introspection of your schema saved as a file by GraphQLSP.
+ * It will automatically be used by `gql.tada` to infer the types of your GraphQL documents.
+ * If you need to reuse this data or update your `scalars`, update `tadaOutputLocation` to
+ * instead save to a .ts instead of a .d.ts file.
+ */
 export type introspection = {
-  // Hier deine introspection Daten einfügen
-  __schema: {
-    queryType: { name: "Query" },
-    mutationType: { name: "Mutation" },
-    subscriptionType: null,
-    types: [],
-    directives: []
-  }
+  name: never;
+  query: 'Query';
+  mutation: 'Mutation';
+  subscription: never;
+  types: {
+    'Boolean': unknown;
+    'ID': unknown;
+    'Mutation': { kind: 'OBJECT'; name: 'Mutation'; fields: { 'createTask': { name: 'createTask'; type: { kind: 'NON_NULL'; name: never; ofType: { kind: 'LIST'; name: never; ofType: { kind: 'OBJECT'; name: 'Task'; ofType: null; }; }; } }; 'deleteTask': { name: 'deleteTask'; type: { kind: 'NON_NULL'; name: never; ofType: { kind: 'LIST'; name: never; ofType: { kind: 'OBJECT'; name: 'Task'; ofType: null; }; }; } }; 'updateTask': { name: 'updateTask'; type: { kind: 'NON_NULL'; name: never; ofType: { kind: 'LIST'; name: never; ofType: { kind: 'NON_NULL'; name: never; ofType: { kind: 'OBJECT'; name: 'Task'; ofType: null; }; }; }; } }; }; };
+    'Query': { kind: 'OBJECT'; name: 'Query'; fields: { 'tasks': { name: 'tasks'; type: { kind: 'NON_NULL'; name: never; ofType: { kind: 'LIST'; name: never; ofType: { kind: 'OBJECT'; name: 'Task'; ofType: null; }; }; } }; }; };
+    'String': unknown;
+    'Task': { kind: 'OBJECT'; name: 'Task'; fields: { 'id': { name: 'id'; type: { kind: 'NON_NULL'; name: never; ofType: { kind: 'SCALAR'; name: 'ID'; ofType: null; }; } }; 'name': { name: 'name'; type: { kind: 'NON_NULL'; name: never; ofType: { kind: 'SCALAR'; name: 'String'; ofType: null; }; } }; }; };
+  };
 };
 
-// import * as gqlTada from 'gql.tada';
+import * as gqlTada from 'gql.tada';
 
 declare module 'gql.tada' {
   interface setupSchema {
-    introspection: introspection;
+    introspection: introspection
   }
 }

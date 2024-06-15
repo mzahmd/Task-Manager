04db2c5a606087654278e957972115c9e59ece67
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-06-15 12:45:27 +0200

    rename function

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -22,14 +22,14 @@ export default function TaskCard({ name, id }: TaskCardProps) {
   const [_, deleteTask] = useMutation(DeleteTaskMutation);
   const [__, updateTask] = useMutation(UpdateTaskMutation);
 
-  async function handleEdit(isEdit: boolean, oldTask?: TaskCardProps) {
+  async function handleEditTask(isEdit: boolean, oldTask?: TaskCardProps) {
     setIsEditing(isEdit);
     if (oldTask) {
       await updateTask({ ...oldTask, name: newTaskName })
     }
   }
 
-  async function handleDelete(id: string) {
+  async function handleDeleteTask(id: string) {
     await deleteTask({ id });
   }
 
@@ -37,11 +37,11 @@ export default function TaskCard({ name, id }: TaskCardProps) {
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mx-auto my-2">
       <input type="text" className="p-1" onChange={(e) => setTaskName(e.target.value)} value={newTaskName} readOnly={!isEditing} />
       {isEditing ?
-        <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEdit(false, { id, name })}><FaRegSave /></button>
+        <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEditTask(false, { id, name })}><FaRegSave /></button>
         :
-        <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEdit(true)}><FaPencilAlt /></button>
+        <button className="font-bold bg-gray-500 hover:bg-gray-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleEditTask(true)}><FaPencilAlt /></button>
       }
-      <button className="font-bold bg-red-500 hover:bg-red-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleDelete(id)}><FaTrash /></button>
+      <button className="font-bold bg-red-500 hover:bg-red-700 rounded text-slate-200 shadow ms-2 p-2" type="button" onClick={() => handleDeleteTask(id)}><FaTrash /></button>
     </div>
   )
 }
--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -9,7 +9,7 @@ export default function Tasks() {
   const [{ data, error, fetching }] = useQuery({ query: TaskQuery })
   const [_, createTask] = useMutation(CreateTaskMutation)
 
-  async function handleAddClick() {
+  async function handleAddTask() {
     if (taskName) {
       await createTask({ name: taskName })
       setTaskName("")
@@ -27,7 +27,7 @@ export default function Tasks() {
   return (
     <div className="my-5">
       <input type="text" className="shadow border border-slate-300 p-1" onChange={(e) => setTaskName(e.target.value)} value={taskName} />
-      <button className="font-bold bg-emerald-500 hover:bg-emerald-700 rounded text-slate-200 shadow ms-2 py-1 px-2" type="button" onClick={handleAddClick}>ADD</button>
+      <button className="font-bold bg-emerald-500 hover:bg-emerald-700 rounded text-slate-200 shadow ms-2 py-1 px-2" type="button" onClick={handleAddTask}>ADD</button>
       {data?.tasks.map((item) => <TaskCard {...item} key={item.id} />)}
     </div>
   )

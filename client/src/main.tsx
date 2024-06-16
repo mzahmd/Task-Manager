1bb642d8eb210fe1691f57d2bd8feb79dfb02e3c
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-06-16 12:14:52 +0200

    format

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,11 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { Provider as UrqlProvider } from "urql";
+
+import App from "./App.tsx";
 import { client } from "./lib/urqlClient.ts";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
